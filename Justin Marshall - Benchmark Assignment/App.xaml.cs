e04dedb296afea7f21e379b712dba0a424b0b73e
N.A <N.A@LAPTOP-SL3F3H70>
2026-04-08 15:34:31 +1300

    Add project files.

---
+++ Justin Marshall - Benchmark Assignment/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Justin_Marshall___Benchmark_Assignment
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
